5d98734975876f3e1780c7b72ec7a3e3811330a1
Mr-HFZ <crpheinerflorez@gmail.com>
2025-05-06 19:10:00 -0500

    Update Pila.py

--- Pila.py
+++ Pila.py
@@ -1,50 +1,59 @@
 from ListaSimple import Nodo
-class Pila:
-    def __init__(self) -> None:
+from pila import Pila  
+
+class Cola:
+    def __init__(self) -> bool:
         self.cab = None
         self.fin = None
-        
-    def estavacia(self) -> bool:
+    
+    def estavacia(self) -> None:
         return self.cab == None
-        
-    def apilar(self, valor) -> None:
+    
+    def mostrarCola(self) -> None:
+        aux = self.cab
+        print("Frente", end=", ")
+        while aux != None:
+            print(aux.valor, end=", ")
+            aux = aux.sig
+        print("Cola")
+    
+    def encolar(self, valor) -> None:
         nodo = Nodo(valor)
         if self.estavacia():
             self.cab = nodo
             self.fin = nodo
         else:
-            nodo.sig = self.cab
-            self.cab = nodo
+            self.fin.sig = nodo
+            self.fin = nodo
     
-    # def desapilar(self) -> None:
-    #     if self.estavacia():
-    #         print("La pila está vacía")
-    #     else:
-    #         aux = self.cab
-            
-    def tope(self) -> None:
-        return self.cab.valor
-    
-    def desapilar(self)->None:
+    def desencolar(self) -> None:
         if self.estavacia():
             return None
-        eliminado=self.cab
+        eliminado = self.cab
         
         if self.cab == self.fin:
-            self.cab=None
-            self.fin=None
+            self.cab = None
+            self.fin = None
         else:
-            self.cab=self.cab.sig
+            self.cab = self.cab.sig
         
-        eliminado.sig = None
-        return eliminado
-        
+        eliminado.sig = None    
+        return eliminado.valor
     
-    #mostrar pila no es una funcion oficial
-    def mostrarPila(self) -> None:
-        aux = self.cab
-        print("Tope")
-        while aux != None:
-            print(aux.valor)
-            aux = aux.sig
-            
+    def mostrarFrente(self) -> None:
+        if self.estavacia():
+            return None
+        return self.cab.valor
+    
+    def mostrarFin(self) -> None:
+        if self.estavacia():
+            return None
+        return self.fin.valor
+
+    # Método para construir una pila desde la cola
+    def construir_pila_desde_cola(self) -> Pila:
+        pila = Pila()  # Creamos una nueva pila
+        while not self.estavacia():  # Mientras la cola no esté vacía
+            valor = self.desencolar()  # Desencolamos un elemento
+            pila.apilar(valor)  # Apilamos el elemento en la pila
+        return pila
